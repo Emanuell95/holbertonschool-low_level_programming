91d0737c43b88b2ebf38bbebcf0df287aa908b16
Emanuel Mendoza Nieves <emanuelmendozanieves@Emanuels-MacBook-Pro.local>
2024-11-09 10:21:12 -0400

    print string

--- recursion/0-main.c
+++ recursion/0-main.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    _puts_recursion("Puts with recursion");
-    return (0);
+void _puts_recursion(char *s) {
+    if (*s == '\0') {       
+        putchar('\n');      
+        return;             
+    }
+    putchar(*s);            
+    _puts_recursion(s + 1); 
 }
