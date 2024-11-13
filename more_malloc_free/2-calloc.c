42b3f475c2b18e854862687470c9e9e17af7ca45
Emanuel Mendoza Nieves <emanuelmendozanieves@Emanuels-MacBook-Pro.local>
2024-11-13 19:04:29 -0400

    calloc

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,34 +1,34 @@
 #include <stdlib.h>
-#include "main.h"
+
 /**
- *_calloc - Allocates memory for an array and initializes it to zero
- *@nmemb: Number of elements in the array
- *@size: Size of each element in bytes
+ * _calloc - Allocates memory for an array and initializes it to zero
+ * @nmemb: Number of elements in the array
+ * @size: Size of each element in bytes
  *
- *Return: Pointer to the allocated memory. If nmemb or size is 0, or if
+ * Return: Pointer to the allocated memory. If nmemb or size is 0, or if
  *         malloc fails, returns NULL.
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
     unsigned int total_size;
     void *ptr;
-    char *initializer;
     unsigned int i;
+    char *initializer;
 
-    
+    /* Check for zero nmemb or size */
     if (nmemb == 0 || size == 0)
         return (NULL);
 
-   
+    /* Calculate total memory size */
     total_size = nmemb * size;
 
-    
+    /* Allocate memory */
     ptr = malloc(total_size);
     if (ptr == NULL)
         return (NULL);
 
-    
-    initializer = ptr;  // Use a char pointer to set each byte to zero
+    /* Initialize allocated memory to zero */
+    initializer = (char *)ptr;
     for (i = 0; i < total_size; i++)
         initializer[i] = 0;
 
