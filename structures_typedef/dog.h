67c1a4ac222a3d56f87bbe4800a3284bc328a5d0
Emanuel Mendoza Nieves <emanuelmendozanieves@Emanuels-MacBook-Pro.local>
2024-11-16 16:59:29 -0400

    structures

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -3,10 +3,10 @@
 
 
 /**
- * struct dog - dog data
- * @name: name of the dog
- * @age: age of the dog
- * @owner: name of the owner of the dog
+ *struct dog - dog data
+ *@name: name of the dog
+ *@age: age of the dog
+ *@owner: name of the owner of the dog
  */
 typedef struct dog
 {
